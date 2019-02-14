dc687e85b51d7268984f49c2a3d7d810dbc97e07
Garth Webb <garth.webb@gmail.com>
2019-02-14 09:37:38 -0800

    Checkpoint some files

---
+++ photobooth/kiosk.py
@@ -0,0 +1,20 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from photobooth.db import get_db
+
+bp = Blueprint('kiosk', __name__, url_prefix='/kiosk')
+
+
+@bp.route('/ready', methods=('GET', 'POST'))
+def ready():
+    return render_template('kiosk/ready.html')
+
+
+@bp.route('/photo', methods=('GET', 'POST'))
+def photo():
+    return render_template('kiosk/photo.html')
